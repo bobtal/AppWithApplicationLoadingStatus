898921be201878a1ada3acc46d7714ac3032ce32
bobtal <talevski.boban@gmail.com>
2021-03-01 20:45:12 +0100

    Adds a (relatively) static LoadingButton

--- app/src/main/java/com/udacity/LoadingButton.kt
+++ app/src/main/java/com/udacity/LoadingButton.kt
@@ -2,7 +2,7 @@ package com.udacity
 
 import android.animation.ValueAnimator
 import android.content.Context
-import android.graphics.Canvas
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 import kotlin.properties.Delegates
@@ -12,6 +12,13 @@ class LoadingButton @JvmOverloads constructor(
 ) : View(context, attrs, defStyleAttr) {
     private var widthSize = 0
     private var heightSize = 0
+
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.FILL
+        textAlign = Paint.Align.CENTER
+        textSize = 55.0f
+        typeface = Typeface.create("", Typeface.NORMAL)
+    }
 
     private val valueAnimator = ValueAnimator()
 
@@ -28,6 +35,20 @@ class LoadingButton @JvmOverloads constructor(
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
 
+        // Draw the rectangle
+        paint.color = context.getColor(R.color.colorPrimary)
+        canvas?.drawRect(0.0f, 0.0f, widthSize.toFloat(), heightSize.toFloat(), paint)
+
+        // Draw the text
+        paint.color = Color.WHITE
+        val textHeight = paint.descent() - paint.ascent()
+        val textOffset = (textHeight / 2) - paint.descent()
+        canvas?.drawText(
+                resources.getString(R.string.download),
+                widthSize.toFloat()/2,
+                heightSize.toFloat()/2 + textOffset,
+                paint
+        )
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
